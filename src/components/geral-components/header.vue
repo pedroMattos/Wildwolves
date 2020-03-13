ddea44da2e855a1572cdf392870d62c1d2263488
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-03-12 23:09:48 -0300

    create nav-bar component

---
+++ src/components/geral-components/header.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav id="nav" class="row">
+    <div class="col s12">
+      <ul class="container">
+        <li><img src="../images/logo.png" alt=""></li>
+        <li><p>Sobre</p></li>
+        <li><p>Notícias</p></li>
+        <li><p>Loja</p></li>
+        <li><p>Times</p></li>
+        <li><p>Streams</p></li>
+        <li><p>Parceiros</p></li>
+        <li><p>Contato</p></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'header',
+};
+</script>
+
+<style lang="scss">
+@import '../../assets/scss/variables.scss';
+
+  #nav {
+    ul {
+      display: inline-flex;
+      list-style: none;
+      color: $grey-text;
+      li {
+        margin: 0 18px 0;
+        p {
+          margin-top: 50px;
+          &:first-child {
+            margin-left: 10px;
+          }
+        }
+      }
+    }
+    img {
+      margin-top: 30px;
+      margin-right: 100px;
+      width: 182px;
+      height: 58px;
+    }
+  }
+</style>
