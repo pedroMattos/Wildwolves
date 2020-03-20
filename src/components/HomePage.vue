854a4bbd4c084c643f7d35cfbfb9cf1f53ea2d9a
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-03-20 17:57:38 -0300

    add new structure

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="hello">
+  <div id="main-home">
     <nav-bar></nav-bar>
+    <img id="banner" src="./images/Home-wild.png" alt="Home-banner">
+    <news-section></news-section>
+    <champ-section></champ-section>
   </div>
 </template>
 
 <script>
 import header from './geral-components/header.vue';
+import news from './geral-components/News.vue';
+import campeonatos from './geral-components/Campeonatos.vue';
 
 export default {
   name: 'HomePage',
   components: {
     'nav-bar': header,
+    'news-section': news,
+    'champ-section': campeonatos,
   },
   props: {
     msg: String,
@@ -19,5 +26,12 @@ export default {
 </script>
 
 <style lang="scss">
+  #main-home {
+    #banner {
+      width: 100vw;
+      height: 100vh;
+      object-fit: cover;
 
+    }
+  }
 </style>
