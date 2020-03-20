c559b7c18fbd72894d43db3b03ff6f751918ea09
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-03-20 17:58:20 -0300

    add sec-news

---
+++ src/components/geral-components/News.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="container" id="news">
+    <div class="row">
+      <h2 class="col-sm-12 sec-title">Notícias</h2>
+      <div class="col-md-3 col-sm-12">
+        <card-news></card-news>
+      </div>
+      <div class="col-md-3 col-sm-12">
+        <card-news></card-news>
+      </div>
+      <div class="col-md-3 col-sm-12">
+        <card-news></card-news>
+      </div>
+      <div class="col-md-3 col-sm-12">
+        <card-news></card-news>
+      </div>
+      <btn-cta></btn-cta>
+    </div>
+  </section>
+</template>
+
+<script>
+import cardNews from './deep-components/card-new.vue';
+import btnCta from './deep-components/btn-cta.vue';
+
+export default {
+  name: 'new-sec',
+  components: {
+    'card-news': cardNews,
+    'btn-cta': btnCta,
+  },
+};
+</script>
