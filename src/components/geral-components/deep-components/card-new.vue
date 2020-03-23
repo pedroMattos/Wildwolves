6a6948c98d460fd92b91b865167e41345850a86d
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-03-23 00:34:30 -0300

    remove geral style

--- src/components/geral-components/deep-components/card-new.vue
+++ src/components/geral-components/deep-components/card-new.vue
@@ -24,30 +24,5 @@ export default {
       width: 100%;
       object-fit: cover;
     }
-    .text-field {
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      top: 0;
-      margin: 0 0px;
-      color: white;
-      div {
-        height: fit-content;
-        position: absolute;
-        bottom: 0;
-        padding: 10px 10px 10px;
-      }
-      h3 {
-        font-weight: 400;
-        margin: 0;
-        font-size: 18px;
-      }
-      p {
-        margin: 0;
-        font-weight: 200;
-      }
-      background-image: linear-gradient(transparent, rgba(0, 0, 0, 0.616), #000);
-    }
   }
 </style>
