072e00e13b3c0fdb8fba73500d61b651489bb674
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-03-23 00:34:03 -0300

    add streams section structure

---
+++ src/components/geral-components/Streams.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="container" id="streams">
+    <div class="row">
+      <h2 class="col-sm-12 sec-title">Streams</h2>
+      <div class="col-sm-12 col-md-3">
+        <card-stream></card-stream>
+      </div>
+      <div class="col-sm-12 col-md-3">
+        <card-stream></card-stream>
+      </div>
+      <div class="col-sm-12 col-md-3">
+        <card-stream></card-stream>
+      </div>
+      <div class="col-sm-12 col-md-3">
+        <card-stream></card-stream>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import cardStream from './deep-components/card-stream.vue';
+
+export default {
+  name: 'sec-streams',
+  components: {
+    'card-stream': cardStream,
+  },
+};
+</script>
+
+<style lang="scss">
+  #stream .col-sm-12.col-md-3 {
+    padding: 0;
+    margin-left: 15px;
+    margin-right: 15px;
+  }
+</style>
