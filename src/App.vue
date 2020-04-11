cdaab7ba0305fc16782d62d8185d491bc13008d3
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-11 19:12:54 -0300

    import global components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
+    <nav-bar></nav-bar>
     <router-view></router-view>
+    <footer-site></footer-site>
   </div>
 </template>
 
 <script>
+import header from './components/geral-components/header.vue';
+import footer from './components/geral-components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
+    'nav-bar': header,
+    'footer-site': footer,
   },
 };
 </script>
