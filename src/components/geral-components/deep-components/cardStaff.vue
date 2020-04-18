9b16e1838b8fff8d82d6f865b1c92feecf3071c9
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-17 23:12:28 -0300

    create: new component

---
+++ src/components/geral-components/deep-components/cardStaff.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="staff-card">
+    <p>Gabriel Melo</p>
+    <div class="staff-infos">
+      <p>Gabriel</p>
+      <social-fb id="fb"></social-fb>
+      <social-tt id="tt"></social-tt>
+      <social-is id="is"></social-is>
+    </div>
+  </div>
+</template>
+
+<script>
+import socialFb from '../../svg/social-fb.vue';
+import socialTt from '../../svg/social-tt.vue';
+import socialIs from '../../svg/social-is.vue';
+
+export default {
+  name: 'card-staff',
+  components: {
+    'social-fb': socialFb,
+    'social-tt': socialTt,
+    'social-is': socialIs,
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../../../assets/scss/variables.scss';
+  .staff-card {
+    border-bottom: 1px solid $grey-transp;
+    max-width: 190px;
+    position: relative;
+    color: $grey-text;
+    margin-bottom: 50px;
+    &:hover {
+      .staff-infos {
+        &::after {
+          position: absolute;
+          height: 50px;
+          width: 190px;
+          transition-duration: .5s;
+          content: '';
+          background-color: $black-body;
+          z-index: 0;
+        }
+        svg {
+          width: 25px;
+          height: 25px;
+          &:hover {
+            fill: white;
+            transform: scale(1.1);
+            cursor: pointer;
+          }
+          &#tt {
+            right: 150px;
+          }
+          &#fb {
+            right: 90px;
+          }
+          &#is {
+            right: 20px;
+          }
+        }
+      }
+    }
+    p {
+      font-size: 10px;
+      margin: 0;
+    }
+    .staff-infos {
+      width: 100%;
+      &::after {
+        position: absolute;
+        height: 50px;
+        width: 50px;
+        content: '';
+        background-color: $black-body;
+        right: 0;
+        transition-duration: .5s;
+        bottom: 0;
+        z-index: 1;
+      }
+      p {
+        font-size: 14px;
+        margin-bottom: 10px;
+        text-transform: uppercase;
+      }
+      svg {
+        position: absolute;
+        fill: $grey-text;
+        width: 10px;
+        height: 10px;
+        bottom: 10px;
+        z-index: 2;
+        &#tt {
+          right: 24px;
+        }
+        &#fb {
+          right: 12px;
+        }
+        &#is {
+          right: 0;
+        }
+      }
+    }
+  }
+</style>
---
+++ src/components/geral-components/deep-components/mvv.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="container" id="mvv">
+    <div class="row">
+      <div class="col-sm-12 col-md-4">
+        <h2>Missão</h2>
+        <p>Profissionalizar o esports</p>
+      </div>
+      <div class="col-sm-12 col-md-4">
+        <h2>Visão</h2>
+        <p>Chegar ao CBLOL</p>
+      </div>
+      <div class="col-sm-12 col-md-4">
+        <h2>Valores</h2>
+        <p>Acreditamos em algo</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'section-mvv',
+};
+</script>
+
+<style lang="scss">
+@import '../../../assets/scss/variables.scss';
+
+  #mvv {
+    color: $grey-text;
+    margin-bottom: 80px;
+  }
+</style>
