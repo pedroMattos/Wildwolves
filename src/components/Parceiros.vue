d7bf0eb6d65b7adb0c9338bff0af0ecc0ce76e09
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-18 00:01:45 -0300

    add new page

---
+++ src/components/Parceiros.vue
@@ -0,0 +1,43 @@
+<template>
+  <section id="main-parceiros">
+    <h1 style="display:none">Parceiros</h1>
+    <p id="normal-hover" class="centered">
+      <img id="normal" src="./images/parceiros-normal.png" alt="Title Page">
+      <img id="hover" src="./images/parceiros-hover.png" alt="Title Page">
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'page-parceiros',
+};
+</script>
+
+<style lang="scss">
+  #main-parceiros {
+    padding-top: 150px;
+    #normal-hover {
+      position: relative;
+      &:hover {
+        #normal {
+          opacity: 0;
+          transition-duration: .5s;
+        }
+        #hover {
+          opacity: 1;
+          transition-duration: .5s;
+        }
+      }
+      #normal {
+        position: absolute;
+        transition-duration: .5s;
+        opacity: 1;
+      }
+      #hover {
+        opacity: 0;
+        transition-duration: .5s;
+      }
+    }
+  }
+</style>
