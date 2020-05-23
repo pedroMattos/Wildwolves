9c05acfbd758c61b2d595479014adc6f48f44324
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-05-22 21:54:21 -0300

    add streams page

---
+++ src/components/Streams.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="container" id="main-streams">
+    <h1>Streams</h1>
+
+    <card/>
+    <card/>
+    <card/>
+    <card/>
+    <card/>
+    <card/>
+
+  </section>
+</template>
+
+<script>
+import card from './geral-components/CardStream.vue';
+
+export default {
+  name: 'page-streams',
+  components: {
+    card,
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../assets/scss/variables.scss';
+  #main-streams {
+    padding-top: 150px;
+    h1 {
+      text-align: center;
+      color: $grey-text;
+    }
+  }
+</style>
