81e9ea7f9d499e189949b7f1fded77279b83c8cd
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-05-22 21:54:43 -0300

    partners page

---
+++ src/components/geral-components/Partners.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <card-part></card-part>
+      <card-part></card-part>
+      <card-part></card-part>
+      <card-part></card-part>
+      <card-part></card-part>
+      <card-part></card-part>
+    </div>
+  </div>
+</template>
+
+<script>
+import cardPartners from './deep-components/card-parceiros.vue';
+
+export default {
+  name: 'sec-partners',
+  components: {
+    'card-part': cardPartners,
+  },
+};
+</script>
