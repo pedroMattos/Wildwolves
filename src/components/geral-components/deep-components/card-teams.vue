2772ac00478566b0edc556ef67c2631a49353b2e
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-05-23 23:36:34 -0300

    add card team component

---
+++ src/components/geral-components/deep-components/card-teams.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <img src="@/components/images/lol-team.jpg" alt="Team">
+    <img class="game-logo" src="@/components/images/lol-logo.png" alt="game-logo">
+    <p>Masculino</p>
+    <div class="border-card"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'card-teams',
+};
+</script>
