18a23761e341f6b90c51479a704d376d1f1a2e21
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-05-23 23:33:33 -0300

    add index teams component

---
+++ src/components/IndexTeams.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="cards-area">
+    <router-link
+    class="card-teams"
+    :to="{ name: 'TeamsSingle', params: { name: 'lol-masculine' } }">
+      <card-team></card-team>
+    </router-link>
+    <router-link
+    class="card-teams"
+    :to="{ name: 'TeamsSingle', params: { name: 'lol-masculine' } }">
+      <card-team></card-team>
+    </router-link>
+        <router-link
+    class="card-teams"
+    :to="{ name: 'TeamsSingle', params: { name: 'lol-masculine' } }">
+      <card-team></card-team>
+    </router-link>
+        <router-link
+    class="card-teams"
+    :to="{ name: 'TeamsSingle', params: { name: 'lol-masculine' } }">
+      <card-team></card-team>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import cards from './geral-components/deep-components/card-teams.vue';
+
+export default {
+  name: 'index-teams',
+  components: {
+    'card-team': cards,
+  },
+};
+</script>
