0d4ff941f605f63f2cd6ec64f8ff7320da8f342d
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-05-24 00:49:35 -0300

    add page single

---
+++ src/components/SingleTeams.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container" id="single-teams">
+    <h1 class="centered"><img src="@/components/images/lol-logo.png" alt="Logo League Team"></h1>
+    <div class="cards-area">
+      <card-person></card-person>
+      <card-person></card-person>
+      <card-person></card-person>
+      <card-person></card-person>
+    </div>
+  </section>
+</template>
+
+<script>
+import card from './geral-components/deep-components/card-stream.vue';
+
+export default {
+  name: 'teams-single',
+  components: {
+    'card-person': card,
+  },
+};
+</script>
