4bebd9f49ee21375384aae2d85b95d51821db413
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-05-23 23:35:51 -0300

    add  team main component

---
+++ src/components/Times.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="container section" id="main-times">
+    <router-view></router-view>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'teams-page',
+};
+</script>
