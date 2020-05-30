703e46bcc84a582c3b4de331d1a77663ff9960cd
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-05-29 21:31:11 -0300

    comment code

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,20 @@ export default {
     'nav-bar': header,
     'footer-site': footer,
   },
+  // watch: {
+  //   $route() {
+  //     if(this.screenW == 'smartphone') {
+  //       const controller = document.getElementById('menu-controller');
+  //       // const close = document.getElementById('menu-controller-close');
+  //       const menu = document.getElementById('menu-collapsed');
+  //       const ctx = this;
+  //       if(menu.classList.contains('menu-non-collapsed')) {
+  //         menu.classList.remove('menu-non-collapsed');
+  //         controller.children[0].classList.remove('close');
+  //       }
+  //     }
+  //   }
+  // },
 };
 </script>
 
